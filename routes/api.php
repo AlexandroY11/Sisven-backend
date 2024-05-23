88f6160d057c3c4588954ecca8a76be7c26914c1
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-05-23 17:47:26 -0500

    Rutas Api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\PaymodeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('/categories', CategoryController::class);
+Route::apiResource('/customers', CustomerController::class);
+Route::apiResource('/paymodes', PaymodeController::class);
+Route::apiResource('/products', ProductController::class);
